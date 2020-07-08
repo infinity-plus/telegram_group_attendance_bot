92d6c7798956522ca08040265c721c0bffd911f0
infinity-plus <navdhareharsh2001@gmail.com>
2020-07-09 05:14:44 +0530

    Remove delete message and change list from id to username

--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ class attendance_bot:
     def __init__(self, config):
         self.TOKEN = config.bot_api
         self.flag = 0
+        self.user_list = []
     
     def initialize(self):
         updater = Updater(token=self.TOKEN, use_context=True)
@@ -33,9 +34,7 @@ class attendance_bot:
         self.flag += 1
         if self.flag != 1:
             update.message.reply_text("Please close the current attendance first")
-            update.message.delete()
             return
-        context.chat_data['list'] = []
         original_member = context.bot.get_chat_member(update.effective_chat.id, update.effective_user.id)
         if original_member['status'] in ('creator', 'administrator'):
             keyboard = [[InlineKeyboardButton("Present", callback_data='present')]]
@@ -44,23 +43,22 @@ class attendance_bot:
         else:
             update.message.reply_text("Only admins can execute this command")
 
-        update.message.delete()
-
     @run_async
     def mark_attendance(self, update, context):
         query = update.callback_query
         choice = query.data
         if choice == 'present':
-            context.chat_data['list'].append(update.effective_user.id)
+            self.user_list.append('@'+update.effective_user.username)
             context.bot.answer_callback_query(callback_query_id=query.id, text="Your attendance has been marked", show_alert=True)
 
     def end_attendance(self, update, context):
         original_member = context.bot.get_chat_member(update.effective_chat.id, update.effective_user.id)
         if original_member['status'] in ('creator', 'administrator'):
-            context.bot.edit_message_text(text="Attendance is over. {} people(s) marked attendance. Here is the list. {}".format(len(context.chat_data['list']), 
-                                            context.chat_data['list']), chat_id=self.message.chat_id, message_id=self.message.message_id)
+            str1=("\n".join(self.user_list))
+            context.bot.edit_message_text(text="Attendance is over. {} people(s) marked attendance. Here is the list: \n {}".format(len(self.user_list), 
+                                            str1), chat_id=self.message.chat_id, message_id=self.message.message_id)
         self.flag = 0
-        update.message.delete()
+        self.user_list=[]
 
 if __name__ == '__main__':
 
