9918f8ef211baa3dfcadc99ee779ccc9f7beff72
infinity-plus <navdhareharsh2001@gmail.com>
2020-07-23 20:37:08 +0530

    :sparkles: Use inline keyboard to end attendance -Reduces spam -This removes 'end_attendance' function and handler
    Signed-off-by: infinity-plus <navdhareharsh2001@gmail.com>

--- bot.py
+++ bot.py
@@ -31,13 +31,11 @@ class attendance_bot:
         start_handler = CommandHandler('start', self.start)
         dispatcher.add_handler(start_handler)
         start_attendance_handler = CommandHandler('start_attendance',
-                                                  self.start_attendance, Filters.group)
-        mark_attendance_handler = CallbackQueryHandler(self.mark_attendance)
-        end_attendance_handler = CommandHandler('end_attendance',
-                                                self.end_attendance, Filters.group)
+                                                  self.start_attendance,
+                                                  Filters.group)
+        attendance_handler = CallbackQueryHandler(self.attendance)
         dispatcher.add_handler(start_attendance_handler)
-        dispatcher.add_handler(mark_attendance_handler)
-        dispatcher.add_handler(end_attendance_handler)
+        dispatcher.add_handler(handle_attendance_handler)
 
         # log all errors
         dispatcher.add_error_handler(error)
@@ -57,20 +55,20 @@ class attendance_bot:
         original_member = context.bot.get_chat_member(update.effective_chat.id,
                                                       update.effective_user.id)
         if original_member['status'] in ('creator', 'administrator'):
-            if ('flag' in context.chat_data) and (context.chat_data['flag']
-                                                  == 1):
+            if ('flag' in context.chat_data) and (context.chat_data['flag'] == 1):
                 update.message.reply_text(
                     "Please close the current attendance first")
                 return
-            elif ('flag'
-                  not in context.chat_data) or (context.chat_data['flag']
-                                                == 0):
+            elif ('flag' not in context.chat_data) or (context.chat_data['flag'] == 0):
                 context.chat_data['flag'] = 1
-                context.chat_data['list'] = []
+                context.chat_data['list'] = list()
                 context.chat_data['id'] = update.effective_chat.id
-                keyboard = [[
-                    InlineKeyboardButton("Present", callback_data='present')
-                ]]
+                keyboard = [
+                    [InlineKeyboardButton("Present",
+                                          callback_data='present')],
+                    [InlineKeyboardButton("End Attendance (Admin only)",
+                                          callback_data='end_attendance')]
+                ]
                 reply_markup = InlineKeyboardMarkup(keyboard)
                 self.message = update.message.reply_text(
                     "Please mark your attendance", reply_markup=reply_markup)
@@ -79,41 +77,49 @@ class attendance_bot:
             pass
 
     @run_async
-    def mark_attendance(self, update, context):
+    def attendance(self, update, context):
         query = update.callback_query
         choice = query.data
-        if (choice == 'present') and ('@' + update.effective_user.username
-                                      not in context.chat_data['list']):
-            context.chat_data['list'].append('@' +
-                                             update.effective_user.username)
-            context.bot.answer_callback_query(
-                callback_query_id=query.id,
-                text="Your attendance has been marked",
-                show_alert=True)
-        else:
-            context.bot.answer_callback_query(
-                callback_query_id=query.id,
-                text="Your attendance is already marked",
-                show_alert=True)
-
-    def end_attendance(self, update, context):
-        original_member = context.bot.get_chat_member(update.effective_chat.id,
-                                                      update.effective_user.id)
-        if original_member['status'] in ('creator', 'administrator'):
-            if (context.chat_data['id'] != update.effective_chat.id):
-                return
-            str1 = ("\n".join(context.chat_data['list']))
-            context.bot.edit_message_text(
-                text=
-                "Attendance is over. {} people(s) marked attendance. Here is the list: \n {}"
-                .format(len(context.chat_data['list']), str1),
-                chat_id=self.message.chat_id,
-                message_id=self.message.message_id)
-            context.chat_data['flag'] = 0
-            context.chat_data['list'] = []
-        else:
-            # update.message.reply_text("Only admins can execute this command")
-            pass
+        if (choice == 'present'):
+            if ('@' + update.effective_user.username not in
+                    context.chat_data['list']):
+                context.chat_data['list'].append('@' +
+                                                 update.effective_user.username)
+                context.bot.answer_callback_query(
+                    callback_query_id=query.id,
+                    text="Your attendance has been marked",
+                    show_alert=True)
+            else:
+                context.bot.answer_callback_query(
+                    callback_query_id=query.id,
+                    text="Your attendance is already marked",
+                    show_alert=True)
+        elif (choice == 'end_attendance'):
+            original_member = context.bot.get_chat_member(
+                update.effective_chat.id,
+                update.effective_user.id)
+            if original_member['status'] in ('creator', 'administrator'):
+                if (context.chat_data['id'] != update.effective_chat.id):
+                    return
+                query.answer()
+                str1 = str()
+                for i in context.chat_data['list']:
+                    str1 += i + '\n'
+                context.bot.edit_message_text(
+                    text="Attendance is over. " +
+                    str(len(context.chat_data['list'])) +
+                    " marked attendance.\n" +
+                    "Here is the list:\n" + str1,
+                    chat_id=self.message.chat_id,
+                    message_id=self.message.message_id)
+                context.chat_data['flag'] = 0
+                context.chat_data['list'] = []
+            else:
+                context.bot.answer_callback_query(
+                    callback_query_id=query.id,
+                    text="This command can be executed by admin only",
+                    show_alert=True)
+                pass
 
 
 def main():
